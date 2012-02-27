7a70effba665fe00ff790033283c9d267fb50c72
heiher <admin@heiher.info>
2012-02-27 16:41:27 +0800

    Update: make plugin scriptable

--- src/hev-plugin.c
+++ src/hev-plugin.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <nspr.h>
 #include <npapi.h>
+#include <npruntime.h>
 #include <npfunctions.h>
 #include <gtk/gtk.h>
 
@@ -22,9 +23,29 @@ typedef struct _HevPluginPrivate HevPluginPrivate;
 struct _HevPluginPrivate
 {
 	NPWindow *window;
+	GtkWidget *entry;
+	NPObject *obj;
+};
+
+typedef struct _HevScriptableObj HevScriptableObj;
+
+struct _HevScriptableObj
+{
+	NPObject obj;
+	NPP npp;
 };
 
 static NPNetscapeFuncs *netscape_funcs;
+static NPClass npklass = { 0 };
+static NPIdentifier npi_get_text = { 0 };
+static NPIdentifier npi_set_text = { 0 };
+
+static NPObject * NPO_Allocate(NPP npp, NPClass *klass);
+static void NPO_Deallocate(NPObject *npobj);
+static bool NPO_HasMethod(NPObject *npobj, NPIdentifier method_name);
+static bool NPO_Invoke(NPObject *npobj, NPIdentifier name,
+			const NPVariant *args, uint32_t arg_count,
+			NPVariant *result);
 
 static void g_debug_log_null_handler(const gchar *domain,
 			GLogLevelFlags level, const gchar *message,
@@ -64,6 +85,17 @@ NP_EXPORT(NPError) NP_Initialize(NPNetscapeFuncs *npn_funcs, NPPluginFuncs *npp_
 	npp_funcs->urlnotify = NPP_URLNotify;
 	npp_funcs->event = NPP_HandleEvent;
 	npp_funcs->getvalue = NPP_GetValue;
+
+	/* NP Object Class */
+	npklass.structVersion = NP_CLASS_STRUCT_VERSION;
+	npklass.allocate = NPO_Allocate;
+	npklass.deallocate = NPO_Deallocate;
+	npklass.hasMethod = NPO_HasMethod;
+	npklass.invoke = NPO_Invoke;
+
+	/* NP Object method property identifier */
+	npi_get_text = NPN_GetStringIdentifier("get_text");
+	npi_set_text = NPN_GetStringIdentifier("set_text");
 
 	/* GTK+ Initialize */
 	gtk_init(0, 0);
@@ -126,6 +158,41 @@ void NPN_MemFree(void *ptr)
 	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
 	netscape_funcs->memfree(ptr);
+}
+
+NPIdentifier NPN_GetStringIdentifier(const NPUTF8 *name)
+{
+	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	return netscape_funcs->getstringidentifier(name);
+}
+
+NPUTF8 *NPN_UTF8FromIdentifier(NPIdentifier identifier)
+{
+	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	return netscape_funcs->utf8fromidentifier(identifier);
+}
+
+NPObject *NPN_CreateObject(NPP instance, NPClass *klass)
+{
+	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	return netscape_funcs->createobject(instance, klass);
+}
+
+NPObject *NPN_RetainObject(NPObject *npobj)
+{
+	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	return netscape_funcs->retainobject(npobj);
+}
+
+void NPN_ReleaseObject(NPObject *npobj)
+{
+	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	return netscape_funcs->releaseobject(npobj);
 }
 
 NPError NPP_New(NPMIMEType plugin_type, NPP instance,
@@ -196,7 +263,6 @@ NPError NPP_SetWindow(NPP instance, NPWindow *window)
 {
 	HevPluginPrivate *priv = (HevPluginPrivate*)instance->pdata;
 	GtkWidget *plug = NULL;
-	GtkWidget *entry = NULL;
 
 	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 	
@@ -211,9 +277,9 @@ NPError NPP_SetWindow(NPP instance, NPWindow *window)
 	plug = gtk_plug_new((GdkNativeWindow)window->window);
 	gtk_widget_show(plug);
 
-	entry = gtk_entry_new();
-	gtk_container_add(GTK_CONTAINER(plug), entry);
-	gtk_widget_show(entry);
+	priv->entry = gtk_entry_new();
+	gtk_container_add(GTK_CONTAINER(plug), priv->entry);
+	gtk_widget_show(priv->entry);
 
 	return NPERR_NO_ERROR;
 }
@@ -284,7 +350,12 @@ void NPP_URLNotify(NPP instance, const char *url, NPReason reason,
 NPError NPP_GetValue(NPP instance, NPPVariable variable,
 			void *value)
 {
+	HevPluginPrivate *priv = NULL;
+
 	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	if(instance)
+	  priv = (HevPluginPrivate *)instance->pdata;
 
 	switch(variable)
 	{
@@ -297,11 +368,88 @@ NPError NPP_GetValue(NPP instance, NPPVariable variable,
 	case NPPVpluginNeedsXEmbed:
 		*((PRBool *)value) = PR_TRUE;
 		break;
+	case NPPVpluginScriptableNPObject:
+		if(NULL == priv->obj)
+			priv->obj = NPN_CreateObject(instance, &npklass);
+		else
+		  priv->obj = NPN_RetainObject(priv->obj);
+		*((NPObject **)value)  = priv->obj;
+		break;
 	default:
 		return NPERR_GENERIC_ERROR;
 	}
 
 	return NPERR_NO_ERROR;
+}
+
+static NPObject * NPO_Allocate(NPP npp, NPClass *klass)
+{
+	HevScriptableObj *obj = NULL;
+
+	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	obj = g_slice_new0(HevScriptableObj);
+	obj->obj._class = klass;
+	obj->npp = npp;
+
+	return (NPObject *)obj;
+}
+
+static void NPO_Deallocate(NPObject *npobj)
+{
+	HevScriptableObj *obj = (HevScriptableObj *)npobj;
+
+	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	g_slice_free(HevScriptableObj, obj);
+}
+
+static bool NPO_Invoke(NPObject *npobj, NPIdentifier name,
+			const NPVariant *args, uint32_t arg_count,
+			NPVariant *result)
+{
+	HevScriptableObj *obj = (HevScriptableObj *)npobj;
+	HevPluginPrivate *priv = (HevPluginPrivate *)obj->npp->pdata;
+
+	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	if(name == npi_get_text)
+	{
+		const gchar *text = NULL;
+
+		text = gtk_entry_get_text(GTK_ENTRY(priv->entry));
+		STRINGZ_TO_NPVARIANT(g_strdup(text), *result);
+
+		return true;
+	}
+	else if(name == npi_set_text)
+	{
+		if(1 == arg_count)
+		{
+			GtkEntryBuffer *entry_buffer = NULL;
+			NPString str = NPVARIANT_TO_STRING(args[0]);
+
+			entry_buffer = gtk_entry_get_buffer(GTK_ENTRY(priv->entry));
+			gtk_entry_buffer_set_text(entry_buffer, str.UTF8Characters,
+						str.UTF8Length);
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+static bool NPO_HasMethod(NPObject *npobj, NPIdentifier method_name)
+{
+	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	if(method_name == npi_get_text)
+	  return true;
+	else if(method_name == npi_set_text)
+	  return true;
+
+	return false;
 }
 
 static void g_debug_log_null_handler(const gchar *domain,
